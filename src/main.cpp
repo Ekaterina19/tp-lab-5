bdc02b3c83eeb362463ce98afd2687990ac92529
Ekaterina19 <gorskovaekaterina19@gmail.com>
2019-03-25 00:12:39 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<ctime>
+#include"Student.h"
+#include"Group.h"
+#include"Deanery.h"
+
+int main()
+{
+	// srand(time(0));
+	// Deanery HSE("Pupiles.txt", "groupName.txt");
+	// HSE.getStudent();
+	// HSE.getGroup(30); //true
+	// HSE.getMarks(5);
+	// HSE.setHeadinGroups();
+	// HSE.excludeDad();
+	// HSE.moveStudent("Gorshkova Ekaterina M", HSE.getGroupinD(std::string("PMI")));// false
+	// HSE.moveStudent("Patrick Nathaniel Brooks", HSE.getGroupinD(std::string("P")));//false
+
+	// HSE.getInfo();
+	// HSE.putInfotoFILE("out.txt");
+	
+	
+	// system("pause");
+}
